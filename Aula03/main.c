45b4df885cc529b16a13c7f884a3dfded7ffd784
Matheus Akira Omori <omori@alunos.utfpr.edu.br>
2026-07-01 16:55:06 -0300

    Create main.c

---
+++ Aula03/main.c
@@ -0,0 +1,20 @@
+//3.3
+for(int i = 1; i <= 5; i++){
+    printf("%d\n", i);
+}
+
+//3.4
+#include <stdio.h>
+
+int main(){
+
+    int soma = 0;
+
+    for(int i = 1; i <= 10; i++){
+        soma += i;
+    }
+
+    printf("Soma = %d", soma);
+
+    return 0;
+}
